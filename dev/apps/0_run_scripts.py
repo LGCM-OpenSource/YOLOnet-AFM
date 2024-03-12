e185f3e795e6a44b0d668d70d617619532585885
Edcarlos <ed.silva@hc.fm.usp.br>
2024-03-12 14:01:21 -0300

    :construction: script run all

--- dev/apps/0_run_scripts.py
+++ dev/apps/0_run_scripts.py
@@ -20,16 +20,78 @@ def run_by_operacional_system_ajust(abs_path_file):
 self_absolute_path = os.path.abspath(__file__)
 self = os.path.dirname(self_absolute_path)
 
-scripts_list = [('1_cropping_opt_images.py', 'cropping images...'),
-                ('2_preprocess_unet.py', 'Generating optical AFM image...'),
-                ('3_preprocess_pixel.py', 'Normalizing AFM data file...'),
-                ('4_vUnet_AFM_predictions.py', 'Segmenting cells...'),
-                ('7_eval_model.py', 'Getting general metrics...'),
-                ('8_eval_model_per_cell.py', 'Getting specific metrics...')
-                ]
+scripts_list_dict = {
+                    'CROP_IMG':('1_cropping_opt_images.py', 'cropping images...'),
+                    'PREP_UNET':('2_preprocess_unet.py', 'Generating optical AFM image...'),
+                    'PREP_PIXEL':('3_preprocess_pixel.py', 'Normalizing AFM data file...'),
+                    'PRED_VUNET':('4_vUnet_AFM_predictions.py', 'Segmenting cells...'),
+                    'PRED_UNET':('5_unet_AFM_predict.py', 'Segmenting cells...'),
+                    'PRED_PIXEL':('6_pixel_predict.py', 'Segmenting cells...'),
+                    'EVAL_MODEL':('7_eval_model.py', 'Getting general metrics...'),
+                    'EVAL_CELL':('8_eval_model_per_cell.py', 'Getting specific metrics...')
+                }
+
+
+while True:
+        print(
+        '''
+        Select a number according to which model you want to evaluate:\n
+        0 - All Models
+        1 - vUnet_AFM
+        2 - Unet_AFM
+        3 - Pixel_AFM
+        '''
+        )
+        option  = input('Enter the number of the desired option:\n')
+        
+        if option.isdigit() and int(option) in [0, 1, 2, 3]:
+            option = int(option)
+            break
+        else: 
+            print("Enter a valid option:\n")
+            
+# all models
+if option == 0:
+    run_list = [
+        scripts_list_dict['CROP_IMG'],
+        scripts_list_dict['PREP_UNET'],
+        scripts_list_dict['PREP_PIXEL'],
+        scripts_list_dict['PRED_VUNET'],
+        scripts_list_dict['PRED_UNET'],
+        scripts_list_dict['PRED_PIXEL'],
+        scripts_list_dict['EVAL_MODEL'],
+        scripts_list_dict['EVAL_CELL'],
+    ]
+# vUnet AFM
+elif option == 1:
+    run_list = [
+        scripts_list_dict['CROP_IMG'],
+        scripts_list_dict['PREP_UNET'],
+        scripts_list_dict['PRED_VUNET'],
+        scripts_list_dict['EVAL_MODEL'],
+        scripts_list_dict['EVAL_CELL'],
+    ]
+# Unet AFM
+elif option == 2:
+    run_list = [
+        scripts_list_dict['CROP_IMG'],
+        scripts_list_dict['PREP_UNET'],
+        scripts_list_dict['PRED_UNET'],
+        scripts_list_dict['EVAL_MODEL'],
+        scripts_list_dict['EVAL_CELL'],
+    ]
+# Pixel
+elif option == 3:
+    run_list = [
+        scripts_list_dict['CROP_IMG'],
+        scripts_list_dict['PREP_PIXEL'],
+        scripts_list_dict['PRED_PIXEL'],
+        scripts_list_dict['EVAL_MODEL'],
+        scripts_list_dict['EVAL_CELL'],
+    ]
 
 print('START PROJECT')
-for script, msg in scripts_list:
+for script, msg in run_list:
     time.sleep(2)
     print(f'Start {script}... {msg}\nPlease wait')
     time.sleep(2)
