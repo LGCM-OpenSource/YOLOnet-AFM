674503e18b8a35842468a6f2f7d08c0fbfa44fa3
ArtRocha <arthurhenrique.rocha@gmail.com>
2024-03-15 15:40:23 -0300

    :ok_hand: STYLE 0_run_scripts is reduced and there is a change in the colors of the graph per cell

--- dev/apps/0_run_scripts.py
+++ dev/apps/0_run_scripts.py
@@ -1,127 +1,79 @@
 import os
-import time
 import sys
 import platform
 import subprocess
-import argparse
 from tqdm import tqdm
 
-def run_by_operacional_system_ajust(abs_path_file):
+# Verifica e ajusta o comando de execução conforme o sistema operacional
+def run_by_operating_system(abs_path_file):
     identify = platform.system()
 
     if identify == 'Linux':
-        run_file = subprocess.run([f'{sys.executable} {abs_path_file}'], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
-        #,  stdin=None, stdout=None, stderr=None, close_fds=True -> retornei essas variáveis para rodar novamentde no shell
-        # ,capture_output=True, text=True, -> para salvar a saída
+        run_file =  subprocess.run([f'{sys.executable} {abs_path_file}'], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
     else:
         run_file = subprocess.run([sys.executable,  f'{abs_path_file}'], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
-        
+
     return run_file
 
-self_absolute_path = os.path.abspath(__file__)
-self = os.path.dirname(self_absolute_path)
+# Diretório atual do script
+current_directory = os.path.dirname(os.path.abspath(__file__))
 
+# Dicionário com os scripts a serem executados e suas mensagens correspondentes
 scripts_dict = {
-                    'CROP_IMG':('1_cropping_opt_images.py', 'cropping images...'),
-                    'PREP_UNET':('2_preprocess_unet.py', 'Generating optical AFM image...'),
-                    'PREP_PIXEL':('3_preprocess_pixel.py', 'Normalizing AFM data file...'),
-                    'PRED_VUNET':('4_vUnet_AFM_predictions.py', 'Segmenting cells...'),
-                    'PRED_UNET':('5_unet_AFM_predict.py', 'Segmenting cells...'),
-                    'PRED_PIXEL':('6_pixel_predict.py', 'Segmenting cells...'),
-                    'EVAL_MODEL':('7_eval_model.py', 'Getting general metrics...'),
-                    'EVAL_CELL':('8_eval_model_per_cell.py', 'Getting specific metrics...')
-                }
+    'CROP_IMG': ('1_cropping_opt_images.py', 'cropping images...'),
+    'PREP_UNET': ('2_preprocess_unet.py', 'Generating optical AFM image...'),
+    'PREP_PIXEL': ('3_preprocess_pixel.py', 'Normalizing AFM data file...'),
+    'PRED_VUNET': ('4_vUnet_AFM_predictions.py', 'Segmenting cells...'),
+    'PRED_UNET': ('5_unet_AFM_predict.py', 'Segmenting cells...'),
+    'PRED_PIXEL': ('6_pixel_predict.py', 'Segmenting cells...'),
+    'EVAL_MODEL': ('7_eval_model.py', 'Getting general metrics...'),
+    'EVAL_CELL': ('8_eval_model_per_cell.py', 'Getting specific metrics...')
+}
 
+# Lista de opções de execução
+options = {
+    0: ['CROP_IMG', 'PREP_UNET', 'PREP_PIXEL', 'PRED_VUNET', 'PRED_UNET', 'PRED_PIXEL', 'EVAL_MODEL', 'EVAL_CELL'],
+    1: ['CROP_IMG', 'PREP_UNET', 'PRED_VUNET', 'EVAL_MODEL', 'EVAL_CELL'],
+    2: ['CROP_IMG', 'PREP_UNET', 'PRED_UNET', 'EVAL_MODEL', 'EVAL_CELL'],
+    3: ['CROP_IMG', 'PREP_PIXEL', 'PRED_PIXEL', 'EVAL_MODEL', 'EVAL_CELL']
+}
 
+# Função para executar um script com ou sem argumento
+def run_script(file, msg, arg=None):
+    script_path = os.path.join(current_directory, file)
+
+    if arg is not None and file.split('_')[1] == 'eval':
+        arg_string = f'-op {arg}'
+        script_path = script_path + arg_string
+    else:
+        script_path = script_path
+
+    print(f'START {file}... {msg}\nPlease wait')
+    run_by_operating_system(script_path)
+    print(f'FINISH {file}... {msg}')
+
+# Solicita ao usuário que selecione uma opção
 while True:
-        print(
+    print(
         '''
-        Select a number according to which model you want to evaluate:\n
+        Select a number according to which model you want to evaluate:
+
         0 - All Models
         1 - vUnet_AFM
         2 - Unet_AFM
         3 - Pixel_AFM
         '''
-        )
-        option  = input('Enter the number of the desired option:\n')
-        
-        if option.isdigit() and int(option) in [0, 1, 2, 3]:
-            option = int(option)
-            break
-        else: 
-            print("Enter a valid option:\n")
-            
-# all models
-if option == 0:
-    run_list = [
-        scripts_dict['CROP_IMG'],
-        scripts_dict['PREP_UNET'],
-        scripts_dict['PREP_PIXEL'],
-        scripts_dict['PRED_VUNET'],
-        scripts_dict['PRED_UNET'],
-        scripts_dict['PRED_PIXEL'],
-        scripts_dict['EVAL_MODEL'],
-        scripts_dict['EVAL_CELL'],
-    ]
-# vUnet AFM
-elif option == 1:
-    run_list = [
-        scripts_dict['CROP_IMG'],
-        scripts_dict['PREP_UNET'],
-        scripts_dict['PRED_VUNET'],
-        scripts_dict['EVAL_MODEL'],
-        scripts_dict['EVAL_CELL'],
-    ]
-# Unet AFM
-elif option == 2:
-    run_list = [
-        scripts_dict['CROP_IMG'],
-        scripts_dict['PREP_UNET'],
-        scripts_dict['PRED_UNET'],
-        scripts_dict['EVAL_MODEL'],
-        scripts_dict['EVAL_CELL'],
-    ]
-# Pixel
-elif option == 3:
-    run_list = [
-        scripts_dict['CROP_IMG'],
-        scripts_dict['PREP_PIXEL'],
-        scripts_dict['PRED_PIXEL'],
-        scripts_dict['EVAL_MODEL'],
-        scripts_dict['EVAL_CELL'],
-    ]
+    )
+    option = input('Enter the number of the desired option:\n')
 
-def run_arg_script(file, string , arg = None):
-
-    script = f'{self}{os.sep}{file}'
-
-    # if arg == 0 and file.split('_')[1] == 'eval':
-        
-    #     for i in range(3):
-
-    #         arg_string = f'-op {i+1}'
-    #         scrip_and_args = f'{script} {arg_string}'
-    #         print(f'START {file}... {string}\nPlease wait')
-    #         run_by_operacional_system_ajust(scrip_and_args)
-
-
-    if file.split('_')[1] == 'eval' :
-
-        arg_string = f'-op {arg}'
-        scrip_and_args = f'{script} {arg_string}'
-        print(f'START {file}... {string}\nPlease wait')
-        run_by_operacional_system_ajust(scrip_and_args)
-
+    if option.isdigit() and int(option) in options.keys():
+        option = int(option)
+        break
     else:
-        scrip_and_args = f'{script}'
-        print(f'START {file}... {string}\nPlease wait')
-        run_by_operacional_system_ajust(scrip_and_args)
+        print("Enter a valid option.\n")
 
-    return print(f' FINISH {file}... {string}')
-
+# Execução dos scripts
 print('START PROJECT')
-for script, msg in tqdm(iterable=run_list, colour='green'):
-
-    run_arg_script(file = script, string = msg, arg = option)
-
+for script_key in tqdm(options[option], colour='green'):
+    run_script(scripts_dict[script_key][0], scripts_dict[script_key][1], option)
 print('FINISH PROJECT')
