cf64fd386f125132a0f5cbe894317ab00ecc4fcf
Artrocha <arthurhenrique.rocha@gmail.com>
2024-06-04 10:43:34 -0300

    :ok_hand: DONE plot metrics from test set

--- dev/apps/9_plot_metrics.py
+++ dev/apps/9_plot_metrics.py
@@ -3,26 +3,10 @@ from utils import DataChart
 import os 
 chart = DataChart()
 
-
-df_metrics_list = []
-df_metrics_general_validation = []
-for fold in range(1,11):
-    df_path = f'Unet_AFM_6_channels_{fold}fold_metrics2.csv'
-    df = pd.read_csv(df_path, index_col=0)
-    df_metrics_general_validation.append(df)
-    df = df.replace({'Model':'unet_prediction'},f'unet_{fold}fold')
-    df_metrics_list.append(df)
+# Plot das métricas obtidas no conjunto de teste
+df_path = f'data/output/unet_AFM_predictions1/Unet_AFM_6_channels_metrics.csv'
+df = pd.read_csv(df_path, index_col=0)
+final_df_melt_general = pd.melt(df, id_vars=['Process Date','Model'], value_vars=['Precision', 'Recall', 'F1', 'Dice'], var_name = 'metrics', value_name = 'scores')    
     
-final_df_general_validation = pd.concat(df_metrics_general_validation, axis=0)
-final_df = pd.concat(df_metrics_list, axis=0)
-
-
-final_df_melt_general = pd.melt(final_df_general_validation, id_vars=['Process Date','Model'], value_vars=['Precision', 'Recall', 'F1', 'Dice'], var_name = 'metrics', value_name = 'scores')    
-final_df_melt = pd.melt(final_df, id_vars=['Process Date','Model'], value_vars=['Precision', 'Recall', 'F1', 'Dice'], var_name = 'metrics', value_name = 'scores')
-    
-fig = chart.box_plot(final_df_melt_general, x='Model', y='scores', color = 'metrics', title='Unet_6_channels_on_Test_set')
-fig2 = chart.box_plot(final_df_melt, x='Model', y='scores', color = 'metrics', title='Unet_6_channels_on_Test_set_per_fold')
-
-# fig.show()
-fig.write_image(f'unet_AFM_6_channels_all.png')
-fig2.write_image(f'unet_AFM_6_channels_per_fold.png')
+fig = chart.box_plot(final_df_melt_general, x='Model', y='scores', color = 'metrics', title='Unet_6_channels_final_model')
+fig.write_image(f'unet_AFM_6_channels_final_metrics.png')
