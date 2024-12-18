0bd60fad092d832110901addab6f88088975d8fe
Artrocha <arthurhenrique.rocha@gmail.com>
2024-12-18 17:46:13 -0300

    :bug: FIX txt_files suffix added to read other txt files

--- dev/apps/1_cropping_opt_images.py
+++ dev/apps/1_cropping_opt_images.py
@@ -2,13 +2,14 @@ import os
 from utils import CropImages,build_file_path, CROP_PATH
 from tqdm import tqdm
 
+
 dir = os.listdir(CROP_PATH['optical_raw'])
 
 for img in tqdm(dir, colour='#0000FF'):
     
     image_opt = os.path.join(CROP_PATH['optical_raw'], img)
     image_bw = build_file_path(CROP_PATH['optical_bw_raw'], img, new_process='_OpticalImg-BW.png')
-    txt_dir = build_file_path(CROP_PATH['txt_files'], img, new_process='_2-reference-force-height-extend.txt')
+    txt_dir = build_file_path(CROP_PATH['txt_files'], img, new_process=['_2-reference-force-height-extend.txt', '_2-reference-force-height-extend.jpk-qi-image.txt', '_3-reference-force-height-extend.txt'])
     dest = build_file_path(CROP_PATH['optical_crop_resized'], img, new_process='_optico_crop_resized.png')
 
     imgTrat = CropImages(image_opt, image_bw)
